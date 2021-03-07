e2d1fc2d34f43dbca387ef3517aec73a36828361
deephunt3r <rakeshvaddi1116@gmail.com>
2021-03-07 11:39:51 +0530

    new file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bash
 ## bash - text book to learn [bash_textbook](https://www.gnu.org/software/bash/manual/bash.pdf)
+
--- touch.sh
+++ touch.sh
@@ -1,3 +1,22 @@
 #! /usr/bin/bash/
 echo "hello world!!"
+
 echo "bash is some what similar to c"
+
+
+#Initializing two variables 
+a=10 
+b=20 
+
+#Check whether they are equal 
+if [ $a == $b ] 
+then 
+	echo "a is equal to b"
+
+
+#Check whether they are not equal 
+elif [ $a != $b ] 
+then 
+	echo "a is not equal to b"
+fi 
+
